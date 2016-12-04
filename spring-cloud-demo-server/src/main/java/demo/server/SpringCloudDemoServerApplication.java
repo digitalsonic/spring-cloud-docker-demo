858ab047cf0c27333bc49ccdd637c69009d1862f
DigitalSonic <digitalsonic.dxf@gmail.com>
2016-12-04 19:55:09 +0800

    add simple integration test

--- spring-cloud-demo-server/src/main/java/demo/server/SpringCloudDemoServerApplication.java
+++ spring-cloud-demo-server/src/main/java/demo/server/SpringCloudDemoServerApplication.java
@@ -6,21 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @RestController
 public class SpringCloudDemoServerApplication {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudDemoServerApplication.class, args);
+    @RequestMapping("/hi/{name}")
+    public String hi(@PathVariable String name) {
+        return "Hi " + name;
     }
 
     @RequestMapping("/greeting/{name}")
@@ -28,5 +27,9 @@ public class SpringCloudDemoServerApplication {
         ServiceInstance instance = discoveryClient.getLocalServiceInstance();
         return String.format("Hello %s from %s(%s:%s)", name,
                 instance.getServiceId(), instance.getHost(),instance.getPort());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemoServerApplication.class, args);
     }
 }
---
+++ spring-cloud-demo-server/src/test/java/demo/server/SpringCloudDemoServerApplicationTest.java
@@ -0,0 +1,38 @@
+package demo.server;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudDemoServerApplicationTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void testHi() throws Exception {
+        mockMvc.perform(get("/hi/digitalsonic"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hi digitalsonic"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN));
+    }
+}
