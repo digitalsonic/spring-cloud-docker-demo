70a3c8294249877b4f241ec3ea0036d895dd5873
DigitalSonic <digitalsonic.dxf@gmail.com>
2016-12-11 16:21:08 +0800

    refactor the server part

--- spring-cloud-demo-server/src/main/java/demo/server/GreetingServicesController.java
+++ spring-cloud-demo-server/src/main/java/demo/server/GreetingServicesController.java
@@ -1,7 +1,26 @@
 package demo.server;
 
-/**
- * Created by digitalsonic on 2016/12/5.
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingServicesController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/hi/{name}")
+    public String hi(@PathVariable String name) {
+        return "Hi " + name;
+    }
+
+    @RequestMapping("/greeting/{name}")
+    public String greeting(@PathVariable String name) {
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        return String.format("Hello %s from %s(%s:%s)", name,
+                instance.getServiceId(), instance.getHost(),instance.getPort());
+    }
 }
--- spring-cloud-demo-server/src/main/java/demo/server/SpringCloudDemoServerApplication.java
+++ spring-cloud-demo-server/src/main/java/demo/server/SpringCloudDemoServerApplication.java
@@ -1,34 +1,14 @@
 package demo.server;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
+@EnableResourceServer
 public class SpringCloudDemoServerApplication {
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/hi/{name}")
-    public String hi(@PathVariable String name) {
-        return "Hi " + name;
-    }
-
-    @RequestMapping("/greeting/{name}")
-    public String greeting(@PathVariable String name) {
-        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
-        return String.format("Hello %s from %s(%s:%s)", name,
-                instance.getServiceId(), instance.getHost(),instance.getPort());
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudDemoServerApplication.class, args);
     }
